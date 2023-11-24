0fb756a5a59e3a18d6e69b4de70e18a1804b0522
Luis <luisgouveia74@gmail.com>
2023-11-24 15:10:58 -0300

    implementação de validação de Usuario e Senha

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,9 +13,35 @@ class ValidaFormulario {
 
     handleSubmit(e) {
         e.preventDefault();
-        this.camposSaoValidos();
+       const camposValidos = this.camposSaoValidos();
+       const senhasValidas = this.senhasSaoValidas();
+
+       if(camposValidos && senhasValidas) {
+        alert('Formulario enviado.');
+        this.formulario.submit();
+       }
     }
 
+    senhasSaoValidas(){
+        let valid = true;
+
+        const senha = this.formulario.querySelector('.senha');
+        const repetirSenha = this.formulario.querySelector('.repetir-senha');
+        
+        if (senha.value !== repetirSenha.value) {
+            valid = false;
+            this.criaErro(senha, 'Campos senha e repetir senha precisam ser iguais');
+            this.criaErro(senha, 'Campos senha e repetir senha precisam ser iguais.');
+        }
+
+        if(senha.value.length < 6 || senha.value.length > 12) {
+            valid = false;
+            this.criaErro(senha, 'Senha precisa estar entre 6 e 12 caracteres.');
+        }
+
+        return valid;
+    }
+    
     camposSaoValidos() {
         let valid = true;
 
@@ -34,8 +60,29 @@ class ValidaFormulario {
                 if(!this.validaCPF(campo)) valid = false;
             }
 
-
+            if(campo.classList.contains('usuario')){
+                if(!this.validaUsuario(campo)) valid = false;
+            }
         }
+
+        return valid;
+    }
+
+    validaUsuario(campo){
+        const usuario = campo.value;
+        let valid = true
+        
+        if(usuario.length < 3 || usuario.length > 12) {
+           this.criaErro(campo, 'Usuário precisa ter entre 3 e 12 caracteres');
+           valid = false;     
+        }
+
+        if(!usuario.match(/[a-zA-Z0-9]+/g)) {
+            this.criaErro(campo, 'Nome de úsuario precisa conter apenas letras e/ou numeros');
+            valid = false;     
+        }
+
+        return valid;
     }
 
     validaCPF(campo) {
